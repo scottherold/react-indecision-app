ec5449a32fac4a9ab725103143d4bb2850e73858
Scott Herold <sherold@mail.usf.edu>
2019-12-18 15:44:42 -0500

    JSX Templating, Expanded

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,12 +3,58 @@
 console.log('App.js is running');
 
 // *** JSX VARIABLES *** //
+// ** Templates ** //
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Scott Herold'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 32'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Baltimore'
+    )
+);
+
+// *** ELEMENTS *** //
 var appRoot = document.querySelector('#app');
 
 // *** RENDERING *** //
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 console.log('App.js is running');
 
 // *** JSX VARIABLES *** //
-const template = <h1>Indecision App</h1>
+// ** Templates ** //
+const template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const templateTwo = (
+    <div>
+        <h1>Scott Herold</h1>
+        <p>Age: 32</p>
+        <p>Baltimore</p>
+    </div>
+);
+
+// *** ELEMENTS *** //
 const appRoot = document.querySelector('#app');
 
 // *** RENDERING *** //
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
