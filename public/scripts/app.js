9b6ee953f23632de58a379ec6583efc55ec05a1e
Scott Herold <sherold@mail.usf.edu>
2019-12-20 06:12:09 -0500

    ES6 Classes Playground

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,41 @@
 'use strict';
 
-console.log('built-it-js is running');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// *** VARIABLES *** //
-// ** App Variables ** //
-var app = {
-    title: 'Visibility Toggle',
-    details: 'Hey. These are some details you can now see!'
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var visibility = false;
+// *** Playing with JS Classes *** //
 
-// ** ELEMENTS ** //
-var appRoot = document.querySelector('#app');
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-// *** FUNCTIONS *** //
-// rendering
-var render = function render() {
-    // * JSX Templates * //
-    var jsx = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            app.title
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleVisibility },
-            visibility ? 'Hide Details' : 'Show Details'
-        ),
-        visibility && React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'p',
-                null,
-                app.details
-            )
-        )
-    );
+        _classCallCheck(this, Person);
 
-    // * RENDERING * //
-    ReactDOM.render(jsx, appRoot);
-};
+        this.name = name;
+        this.age = age;
+    }
 
-// toggle visibility
-var toggleVisibility = function toggleVisibility() {
-    visibility = !visibility;
-    render();
-};
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi, I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old.';
+        }
+    }]);
 
-// *** RENDERING *** //
-render();
+    return Person;
+}();
+
+;
+
+var me = new Person('Andrew', 27);
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getDescription());
---
+++ src/playground/es6-classes.js
@@ -0,0 +1,20 @@
+// *** Playing with JS Classes *** //
+
+class Person {
+    constructor(name = 'Anonymous', age = 0) {
+        this.name = name;
+        this.age = age;
+    };
+    getGreeting() {
+        return `Hi, I am ${this.name}!`;
+    };
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`
+    };
+};
+
+const me = new Person('Andrew', 27);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getDescription());
