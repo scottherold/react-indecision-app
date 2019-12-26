9012c5ea1e8cd45d9dd42aa5745493c7d462434a
Scott Herold <sherold@mail.usf.edu>
2019-12-26 16:07:36 -0500

    Babel config in webpack

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 // ** This is the application entry point *** //
 
 // *** MODULES *** //
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person';
+// ** npm Modules ** //
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(4,4));
-// console.log(isAdult(16));
-// console.log(isAdult(20));
-// console.log(canDrink(16));
-// console.log(canDrink(22));
-console.log(isSenior(65));
+// *** JSX *** //
+const template = <p>Hello World!</p>;
+
+// *** RENDERING *** //
+ReactDOM.render(template, document.querySelector('#app'));
