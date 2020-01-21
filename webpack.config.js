4bc718d0b749bf3d040c22ea8fde7c422c3ad6e2
Scott Herold <sherold@mail.usf.edu>
2020-01-21 10:35:07 -0500

    Production Build

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,33 @@
 const path = require('path');
 
 // *** EXPORTS *** //
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/, // <-- supports both css and scss files
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+    const isProduction = env === 'production'; // <-- Checks to see production or development environment
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/, // <-- supports both css and scss files
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist'
+        }
     }
 };
